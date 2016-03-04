0f88597e92266c2f9c1ed29a4d84e32112b0562f
Hristo Georgiev <hgeorgievkzr@gmail.com>
2016-03-04 16:52:55 +0200

    reports II

--- src/app/controllers/buildings/cashbooks/building.cashbook.reports.controller.js
+++ src/app/controllers/buildings/cashbooks/building.cashbook.reports.controller.js
@@ -8,16 +8,37 @@
     .module('kedron')
     .controller('BuildingCashbookReportsController', BuildingCashbookReportsController );
 
-  function BuildingCashbookReportsController(Expense , Deposit , QueryConstructor , $scope) {
+  function BuildingCashbookReportsController(Expense , Deposit , QueryConstructor , $scope , $stateParams) {
     var vm = this;
 
 
     $scope.$watch('cbr.Date', function() {
+      vm.depositsTotal = 0;
+      vm.expensesTotal = 0;
+
       if(vm.Date) {
-        vm.toDate = vm.Date;
-        vm.toDate.setMonth(vm.Date.getMonth() + 1);
-        console.log(vm.toDate);
+        vm.toDate = new Date(vm.Date.getFullYear(), vm.Date.getMonth() + 1, 0);
+
+        var params  = { building_id: $stateParams.buildingId, dateMadeLowerBoundary: vm.Date , dateMadeUpperBoundary: vm.toDate};
+
+        Deposit.query(params  , function(response) {
+          vm.deposits = response.Items;
+          angular.forEach(vm.deposits , function(value,key) {
+
+            vm.depositsTotal += value.Value;
+          });
+          vm.totalDeposits = response.depositCount;
+        });
+        Expense.query(params , function(response) {
+          vm.expenses = response.Items;
+          angular.forEach(vm.expenses , function(value,key) {
+            vm.expensesTotal += value.Value;
+          });
+          vm.totalExpenses = response.expenseCount;
+        })
+
       }
+
 
     });
 
